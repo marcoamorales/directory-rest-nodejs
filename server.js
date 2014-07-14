f7702b792ea235c4c9e59d5ada44f0878b2261a1
Marco A Morales <marcoamorales@gmail.com>
2014-07-14 16:51:09 -0500

    fixed the port logging message.

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.get('/employees', wines.findAll);
 
 console.log("Port server: " + config.node.port);
 app.listen(config.node.port);
-console.log('Listening on port 3000...');
+console.log('Listening on port '+ config.mongo.port + '...');
